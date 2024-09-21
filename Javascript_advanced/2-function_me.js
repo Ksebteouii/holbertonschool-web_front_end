3012f819970cb85ef7c2a4640bfd191c0c514419
sebteouii <sebteouiikhoubayb@gmail.com>
2024-09-21 16:42:53 +0200

    Inside welcomeMessage, an inner function called displayAlert is defined

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,10 @@
+function welcomeMessage(fullName){
+    function displayAlert(){
+        alert("Welcome " + fullName+ "!");
+        }
+        displayAlert();
+}
+// Call the function with the specified names
+welcomeMessage("Guillaume");
+welcomeMessage("Alex");
+welcomeMessage("Fred");
