ea289376ed904aad911afbc96fd3fad5cf9c1f4e
sebteouii <sebteouiikhoubayb@gmail.com>
2024-09-22 17:55:24 +0200

    Execution stack & timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,31 @@
+function countPrimeNumbers() {
+    let primeCount = 0;
+  
+    function isPrime(num) {
+      if (num < 2) return false;
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+      }
+      return true;
+    }
+  
+    for (let i = 2; i <= 100; i++) {
+      if (isPrime(i)) {
+        primeCount++;
+      }
+    }
+  
+    return primeCount;
+  }
+  
+  // Measure the execution time for 100 executions
+  const startTime = performance.now(); // Get start time
+  
+  for (let i = 0; i < 100; i++) {
+    countPrimeNumbers(); // Execute the function 100 times
+  }
+  
+  const endTime = performance.now(); // Get end time
+  
+  const executionTime = endTime - startTime; // Calculate time difference
+  console.log(`Execution time of executing countPrimeNumbers 100 times was ${executionTime} milliseconds`);
